34e2429a8d39caa511fb1eaa2a80e23b41c587f6
Chris Workman <WorkmC@medinsight.milliman.com>
2017-03-24 12:15:09 -0700

    Adding first apply operator for lists

---
+++ DeployCube/DeployCube/Operators.fs
@@ -0,0 +1,8 @@
+﻿module Operators
+
+    module List =
+                
+    let apply (fList: ('a->'a) list) (valList: 'a list) = 
+        fList 
+        |> List.collect (fun x -> valList 
+                                  |> List.map (fun y -> x y))
