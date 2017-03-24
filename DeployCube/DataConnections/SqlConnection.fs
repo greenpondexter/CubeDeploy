aa32d5a0fa90f979310b4972940eaaf54b889206
Chris Workman <WorkmC@medinsight.milliman.com>
2017-03-24 12:39:01 -0700

    Adding DataConnections provider proj.  This should durastically improve build times as TypeProviders has to requery the database on each build.

---
+++ DeployCube/DataConnections/SqlConnection.fs
@@ -0,0 +1,10 @@
+﻿module DataConnections.SqlConnection
+
+open Microsoft.FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq
+
+
+type dbSchema = SqlDataConnection<LocalSchemaFile = "MI_105_HCC17.dbml", ForceUpdate = false, ConnectionString = @"Data Source=topsdev02;Initial Catalog=MI_105_ETG;Integrated Security=SSPI;">
+let db = dbSchema.GetDataContext()
+
+
---
+++ DeployCube/DataConnections/Tables.fs
@@ -0,0 +1,8 @@
+﻿module DataConnections.Tables
+
+open DataConnections.SqlConnection
+
+let miParmsTable = db.MI_PARMS
+let getCubeVer = db.ASRPT_FN_GET_CUBE_VER().GetValueOrDefault()
+let aggDesignTable = db.ASRPT_AS_METADATA_AGGREGATIONDESIGN
+let aggTable = db.ASRPT_AS_METADATA_AGGREGATION
